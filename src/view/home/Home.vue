13136f942cfc91224cae29cebb54e431d8245c86
lc <648956809@qq.com>
2020-05-29 15:39:23 +0800

    首页页面布局样式调整

--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="home">
-        home
+        <el-container class="container">
+        <el-header class="header">Header</el-header>
+        <el-container>
+            <el-aside class="aside" width="200px">Aside</el-aside>
+            <el-main class="main">Main</el-main>
+        </el-container>
+        </el-container>
     </div>
 </template>
 
@@ -16,4 +22,16 @@ export default {
 </script>
 
 <style scoped>
+    .container {
+        height: 100vh;
+    }
+    .header {
+        background-color: #b3c0d1;
+    }
+    .aside {
+        background-color: #d3dce6;
+    }
+    .main {
+        background-color: #e9eef3;
+    }
 </style>
