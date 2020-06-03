b5e41abbb5b972b552ad33ebd62418e970da2038
lc <648956809@qq.com>
2020-06-03 15:04:39 +0800

    用户列表-面包屑修改

--- src/view/user/User.vue
+++ src/view/user/User.vue
@@ -2,7 +2,7 @@
     <el-card class="box-card">
         <!-- 1.面包屑 -->
         <el-breadcrumb separator="/">
-            <el-breadcrumb-item>首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
             <el-breadcrumb-item>用户管理</el-breadcrumb-item>
             <el-breadcrumb-item>用户列表</el-breadcrumb-item>
         </el-breadcrumb>
